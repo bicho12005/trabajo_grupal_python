4bccaa328b8239a23554ba1f025ec20ade950ec3
bicho12005 <hugo126@yahoo.com.ar>
2023-05-27 03:47:12 -0300

    27_5_23

---
+++ assets/JS/scservicios.js
@@ -0,0 +1,19 @@
+    function mostrar_adop(){
+    document.getElementById('adop').style.display ='block';
+    }
+    function ocultar_adop(){
+        document.getElementById('adop').style.display ='none';
+    }
+    function mostrar_guar(){
+        document.getElementById('guar').style.display ='block';
+    }
+    function ocultar_guar(){
+        document.getElementById('guar').style.display ='none';
+    }
+    function mostrar_vet(){
+        document.getElementById('vet').style.display ='block';
+    }
+    function ocultar_vet(){
+        document.getElementById('vet').style.display ='none';
+    }
+
